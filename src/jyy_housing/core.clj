082655940062897e7a22d9e6d45ca57e3c8c2aea
Ville Toiviainen <villej.toiviainen@gmail.com>
2015-11-26 19:27:50 +0200

    Nested ad-list bug removed, more verbose logging

--- src/jyy_housing/core.clj
+++ src/jyy_housing/core.clj
@@ -19,6 +19,7 @@
 (def ^:dynamic *last-result* #{1 2 3})
 
 (defn get-content [elem]
+  (log/info "get-content")
   (first (:content elem)))
 
 (defn parse-title [raw-title]
@@ -26,9 +27,11 @@
     (filter string? (:content raw-title))))
 
 (defn parse-content [raw-content]
+  (log/info "parse-content")
   (get-content raw-content))
 
 (defn parse-details [raw-details]
+  (log/info "parse-details")
   (let [aparment-details (filter string? (:content (first raw-details)))
         landlord-details (filter string? (:content (last raw-details)))]
     (zipmap
@@ -38,6 +41,8 @@
       (concat aparment-details landlord-details)))))
 
 (defn parse-date [raw-date]
+  (log/info "parse-date")
+  (log/info raw-date)
   (clojure.string/replace (get-content raw-date) "\n" " "))
 
 (defn fetch-url [url]
@@ -47,6 +52,8 @@
   (html/select (fetch-url *base-url*) [:#apartment-r-]))
 
 (defn get-ad-data [ad]
+  (println ad)
+  (log/info "get-ad-data")
   (let [title (parse-title (first (html/select [ad] *title-selector*)))
         content (parse-content (first (html/select [ad] *content-selector*)))
         details (parse-details (html/select [ad] *details-selector*))
@@ -56,6 +63,7 @@
        [title content details date])))
 
 (defn set-mail-auth-variables []
+  (log/info "set-mail-auth-variables")
   (gmail/set-client-secret! (environ/env :mail-client-secret))
   (gmail/set-client-id! (environ/env :mail-client-id))
   (gmail/set-refresh-token! (environ/env :mail-refresh-token)))
@@ -63,10 +71,9 @@
 (defn concat-details [details]
   (clojure.string/join " " details))
 
-(defn base64->base64url [mail]
-  (clojure.string/replace mail #"\+|\=|\/" {"+" "-" "=" "*" "/" "_"}))
-
 (defn format-mail [ad]
+  (log/info "Format mail")
+  (log/info ad)
   (clojure.string/join "\n" [(:title ad)
                              (:content ad)
                              (str (concat-details (vals (:details ad))) "\n" (:date ad))]))
@@ -75,6 +82,8 @@
   (str "\n\n" (repeat 20 "*") "\n\n"))
 
 (defn format-mail-seq [ad-seq]
+  (log/info "Format mail seq")
+  (log/info ad-seq)
   {:to "villej.toiviainen@gmail.com"
    :subject "Clojure test"
    :body (clojure.string/join (get-ad-split-string) (map format-mail ad-seq))})
@@ -83,6 +92,7 @@
 (defn send-mail [ad-seq]
   (let [mail (format-mail-seq ad-seq)]
     (log/info "Sending mail to")
+    (log/info mail)
     (gmail/message-send mail)))
 
 (defn compare-latests-results [latest-result]
@@ -94,8 +104,9 @@
    (def *last-result* result))
 
 (defn handle-scrape-result [ad-list]
+  (log/info "handle-scrape-result")
   (let [result (set (map get-ad-data ad-list))]
-    (log/info "handle-scrape-result")
+    (log/info result)
     (if (empty? *last-result*)
       (set-last-result result)
       (send-mail (compare-latests-results result)))))
@@ -104,7 +115,7 @@
 (defn -main
   [& args]
   (log/info "Starting the app")
-  (let [ad-list [(select-jyy-apartment-ads)]]
+  (let [ad-list (select-jyy-apartment-ads)]
     (log/info "Setting mail client env")
     (set-mail-auth-variables)
     (handle-scrape-result ad-list)))
